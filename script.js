ba90d0c5058b1ac864ffba1d85bf20fa6a732e4f
Philip Harberts <phil@Philips-MacBook-Pro.local>
2023-03-01 23:31:12 -0600

    A JS Modal

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const closeModalBtn = document.querySelector('.close-modal');
+
+const openModalBtns = document.querySelectorAll('.show-modal');
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+for(let i = 0; i < openModalBtns.length; i++)
+    openModalBtns[i].addEventListener('click', openModal);
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
